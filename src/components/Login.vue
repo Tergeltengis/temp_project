fc6b04a813491182256d99d41c8b301bd995ace5
tergeltengis <tergeltengis77@gmail.com>
2021-03-11 17:18:12 +0800

    first commit

---
+++ src/components/Login.vue
@@ -0,0 +1,107 @@
+<template>
+  <section class="section-container">
+ 
+  
+
+    <v-row class="signin">
+      <Navbar/>
+      <v-col cols="3" class="login">
+  
+           <img class="logo" src="../assets/logo.svg">
+  
+        <validation-observer ref="observer">
+          <v-form @submit.prevent="submit">
+            <validation-provider v-slot="{ errors }" name="Name" rules="required|email">
+              <v-text-field
+                v-model="email"
+                :error-messages="errors"
+                label="Нэвтрэх нэр"
+                required
+                outlined
+                
+                filled
+                dense
+              ></v-text-field>
+            </validation-provider>
+            <validation-provider v-slot="{ errors }" name="email" rules="required">
+              <v-text-field
+                v-model="password"
+                :error-messages="errors"
+                label="Нууц үг"
+                :append-icon="showPass ? 'mdi-eye' : 'mdi-eye-off'"
+                @click:append="showPass = !showPass"
+                required
+                outlined
+                dense
+                
+                filled
+                :type="showPass ? 'text' : 'password'"
+              ></v-text-field>
+            </validation-provider>
+            <div class="text-center">
+              <v-btn class="signin-btn" type="submit" rounded color="white" dark>
+                Нэвтрэх
+              </v-btn>
+            </div>
+          </v-form>
+        </validation-observer>
+      </v-col>
+    </v-row>
+  </section>
+</template>
+<script>
+import { required, email } from 'vee-validate/dist/rules'
+import { extend, ValidationProvider, setInteractionMode, ValidationObserver } from 'vee-validate'
+import Navbar from './Navbar';
+
+setInteractionMode('eager')
+
+extend('required', {
+  ...required,
+  message: '{_field_} can not be empty'
+})
+
+extend('email', {
+  ...email,
+  message: 'Email must be valid'
+})
+
+export default {
+  components: {
+    Navbar,
+    ValidationProvider,
+    ValidationObserver
+  },
+  data: () => ({
+    email: '',
+    password: null,
+    showPass: false
+  }),
+  computed: {
+    params() {
+      return {
+        email: this.email,
+        password: this.password
+      }
+    }
+  },
+  methods: {
+    async submit() {
+      const valid = await this.$refs.observer.validate()
+      if (valid) {
+        this.login(this.params) // action to login
+      }
+    },
+    clear() {
+      // you can use this method to clear login form
+      this.email = ''
+      this.password = null
+      this.$refs.observer.reset()
+    },
+    login(p) {
+      console.log(p)
+
+    }
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
+import vuetify from '@/plugins/vuetify'
 import App from './App.vue'
+import "./assets/styles.scss";
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  vuetify
 }).$mount('#app')
